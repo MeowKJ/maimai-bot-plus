036a46b30ddeba8c4a01f9215a9b2503cc267726
KJ <ijink@icloud.com>
2024-08-27 19:37:50 +0800

    feat: 增加message.reply引用

--- src/plugins/b50/command.py
+++ src/plugins/b50/command.py
@@ -1,98 +1,45 @@
 import os
 import time
-import asyncio
-import subprocess
 from botpy.message import Message
-from botpy import logger
+from botpy.types.message import Reference
 
 from config import LXNS_API_SECRET
 from src.libraries.database import add_or_update_user, get_user_by_id, update_user_score
 from src.libraries.database.exceptions import DatabaseOperationError, UserNotFoundError
 
-from .tools import is_fish_else_lxns
+from .tools import is_fish_else_lxns, compress_png
 from .player import Player
 from .draw import DrawBest
 
+from botpy import logger
+
+# 定义查分平台的常量
 FISH = 0
 LXNS = 1
 PLATFORM_STR = ["水鱼查分器", "落雪咖啡屋"]
 
 
-async def compress_png(fp, output, force=True, quality=None):
-    """
-    Compresses a PNG file asynchronously.
-
-    Args:
-        fp (str): The file path of the original PNG file.
-        output (str): The file path for the compressed PNG file.
-        force (bool, optional): Whether to force compression. Defaults to True.
-        quality (int or str, optional): Compression quality parameter. Defaults to None.
-
-    Returns:
-        float: Compression ratio percentage.
-    """
-    if not os.path.exists(fp):
-        raise FileNotFoundError(f"File not found: {fp}")
-
-    force_command = "-f" if force else ""
-    quality_command = ""
-
-    if quality and isinstance(quality, int):
-        quality_command = f"--quality {quality}"
-    if quality and isinstance(quality, str):
-        quality_command = f"--quality {quality}"
-
-    command = (
-        f"pngquant {fp} "
-        f"--skip-if-larger {force_command} "
-        f"{quality_command} "
-        f"--output {output}"
-    )
-
-    try:
-        process = await asyncio.create_subprocess_shell(
-            command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-        )
-        stdout, stderr = await process.communicate()
-
-        # 获取原始图像大小
-        original_size = os.path.getsize(fp)
-
-        # 获取压缩后文件的大小
-        compressed_size = os.path.getsize(output)
-
-        # 计算压缩比
-        compression_ratio = (1 - compressed_size / original_size) * 100
-
-        # 检查命令是否成功执行
-        if process.returncode != 0:
-            raise subprocess.CalledProcessError(process.returncode, command)
-
-        return compression_ratio
-    except asyncio.CancelledError as exc:
-        raise exc from None
-    except Exception as e:
-        raise RuntimeError(f"An error occurred: {e}") from e
-
-
+# 处理 /bind 指令的异步函数
 async def handle_bind(message: Message):
+    message_reference = Reference(message_id=message.id)
+
     user_id = int(message.author.id)
-    content = message.content.split("/bind")[1].strip()
+    content = message.content.split("/bind", 1)[-1].strip()
+
+    # 如果用户没有提供绑定信息，返回绑定说明
     if not content:
         await message.reply(
-            content="""
-这里是bind指令的说明：
-/bind后面可以接上水鱼查分器的用户名或者是落雪咖啡屋绑定的QQ号。
-例如：/bind user 将绑定用户名 user。
-
-如果要强制指定平台，可以在用户名后面加上空格：
-- f 表示水鱼查分器
-- l 表示落雪咖啡屋
-
-例如：/bind user f 将绑定用户名到水鱼查分器。
-默认情况下会自动判断平台。
-
-Tip: 输入 / 可以快速唤起我。如有问题请联系频道主。"""
+            content=(
+                "这里是bind指令的说明:\n"
+                "/bind后面可以接上水鱼查分器的用户名或者是落雪咖啡屋绑定的QQ号。\n"
+                "例如：/bind user 将绑定用户名 user。\n\n"
+                "如果要强制指定平台，可以在用户名后面加上空格：\n"
+                "- f 表示水鱼查分器\n"
+                "- l 表示落雪咖啡屋\n\n"
+                "例如：/bind user f 将绑定用户名到水鱼查分器。\n"
+                "默认情况下会自动判断平台。\n\n"
+                "Tip: 输入 / 可以快速唤起我。如有问题请联系频道主。"
+            )
         )
         return
 
@@ -115,30 +62,41 @@ Tip: 输入 / 可以快速唤起我。如有问题请联系频道主。"""
         if not is_fish_else_lxns(user_name):
             platform_id = LXNS
 
-    # 绑定用户
+    # 尝试绑定用户到数据库
     try:
         add_or_update_user(user_id, user_name, platform_id)
     except DatabaseOperationError as e:
         logger.error(f"绑定用户时出错: {e}")
-        await message.reply(content="绑定失败，数据库操作出错。请稍后再试。")
+        await message.reply(
+            content="绑定失败，数据库操作出错。请稍后再试。",
+            message_reference=message_reference,
+        )
         return
 
+    # 成功绑定后回复用户
     await message.reply(
-        content=f"[{user_name}] 已经成功绑定到你的频道号了！查分平台为 [{PLATFORM_STR[platform_id]}]。"
+        content=f"[{user_name}] 已经成功绑定到你的频道号了！查分平台为 [{PLATFORM_STR[platform_id]}]。",
+        message_reference=message_reference,
     )
 
 
+# 处理 /b50 指令的异步函数
 async def handle_b50(message: Message):
+    message_reference = Reference(message_id=message.id)
     start_time = time.time()
     user_id = int(message.author.id)
+
+    # 尝试从数据库获取用户信息
     try:
         username, platform_id, score = get_user_by_id(user_id)
     except Exception:
         await message.reply(
-            content=f"@{message.author.username} 请先使用 /bind 指令绑定你的查分器账号。"
+            content="请先使用 /bind 指令绑定你的查分器账号。",
+            message_reference=message_reference,
         )
         return
 
+    # 初始化玩家对象
     player = Player(
         username,
         user_id,
@@ -146,6 +104,7 @@ async def handle_b50(message: Message):
         api_secret=LXNS_API_SECRET,
     )
 
+    # 获取查分器数据
     try:
         if platform_id == FISH:
             logger.info(f"用户 {user_id} 使用水鱼查分器")
@@ -156,47 +115,69 @@ async def handle_b50(message: Message):
     except Exception as e:
         logger.error(f"获取查分器数据时出错: {e}")
         await message.reply(
-            content=f"@{message.author.username} 获取数据时出错, 检查一下\n1.用户名是否填错了\n2.网站的隐私设置, 是否允许查分器访问\n3.重试几次\n如果进一步遇到问题, 请联系频道主。\n查分器平台: {PLATFORM_STR[platform_id]} 用户名: {username}"
+            content=(
+                "获取数据时出错, 请检查以下事项：\n"
+                "1. 用户名是否填错了\n"
+                "2. 网站的隐私设置，是否允许查分器访问\n"
+                "3. 重试几次\n"
+                "如果进一步遇到问题，请联系频道主。\n\n"
+                f"查分器平台: {PLATFORM_STR[platform_id]} 用户名: {username}"
+            ),
+            message_reference=message_reference,
         )
         return
 
+    # 绘制和压缩图片
     try:
         drawBest = DrawBest(player)
         draw = await drawBest.draw()
 
-        # Save the image to a file
+        # 保存图片到文件
         if not os.path.exists("./tmp"):
             os.makedirs("./tmp")
         image_path = f"./tmp/{username}_b50.png"
         draw.save(image_path)
 
-        # Compress the image
+        # 压缩图片
         compressed_image_path = f"./tmp/{username}_b50_compressed.png"
         compression_ratio = await compress_png(image_path, compressed_image_path)
 
     except Exception as e:
         logger.error(f"绘制或压缩图片时出错: {e}")
         await message.reply(
-            content=f"@{message.author.username} 处理图片时出错, 如果多次出现错误, 请联系频道主。"
+            content="处理图片时出错, 如果多次出现错误, 请联系频道主。",
+            message_reference=message_reference,
         )
         return
 
+    # 更新用户分数到数据库
     try:
         update_user_score(user_id, player.rating)
     except DatabaseOperationError as e:
         logger.error(f"更新用户时出错: {e}")
         return
 
+    # 计算生成时间
     generation_time = time.time() - start_time
 
+    # 回复压缩后的图片
     await message.reply(
-        file_image=compressed_image_path,  # Pass the compressed file path
+        file_image=compressed_image_path,
+        message_reference=message_reference,
     )
+
+    # 回复生成成功信息
     await message.reply(
-        content=f"@{message.author.username} B50[{PLATFORM_STR[platform_id]}]生成成功, 耗时 {generation_time:.2f} 秒。\n压缩比: {compression_ratio:.2f}%\n更多统计信息可以访问 Maimai的网页查分器(详见频道帖子-相关教程)。",
+        content=(
+            f"B50[{PLATFORM_STR[platform_id]}]生成成功, 耗时 {generation_time:.2f} 秒。\n"
+            f"压缩比: {compression_ratio:.2f}%\n"
+            "更多统计信息可以访问 Maimai的网页查分器 (详见频道帖子-相关教程)。"
+        ),
+        message_reference=message_reference,
     )
 
 
+# 空操作函数，用于处理无效指令
 async def do_nothing(message: Message):
     pass
 
